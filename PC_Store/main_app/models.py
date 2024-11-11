f426a67db44695486cbb14c88b9718c366f2a828
Your NameAli-Alhaddar <youremail@domain.comAlaoealhaddar@gmail.com>
2024-11-11 10:05:20 +0300

    v4

--- PC_Store/main_app/models.py
+++ PC_Store/main_app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-
-# Create your models here.
 class Pc(models.Model):
     name = models.CharField(max_length=100, default="")
     description = models.TextField(max_length=250)
@@ -13,8 +11,8 @@ class Pc(models.Model):
     motherboard = models.TextField(max_length=150, default="")
     image = models.ImageField(upload_to='main_app/static/uploads/', default="")
 
-    def get_absolute_url(self):
-        return reverse('detail' , kwargs={'PC_id': self.id})
-    
     def __str__(self):
         return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pc_id': self.id})
